8248364c500201966ac502517fa9f75282272b97
Manon Bosselut <duchmol7@hotmail.com>
2019-02-06 23:16:06 +0100

    Connected to database through mongoose

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var bodyParser = require("body-parser");
 //mongodb test
 const mongoose = require('mongoose'); // requiring our package
 
-mongoose.connect('mongodb://localhost/test_2'); // establishing the connection
+mongoose.connect('mongodb://localhost/dinner-library-test'); // establishing the connection
 mongoose.connection
 .once('open', () => console.log('Connection established'))
 .on('error', (error) => {
@@ -17,28 +17,57 @@ app.use(bodyParser.urlencoded({extended: true}));
 //adds .ejs to all route names
 app.set("view engine", "ejs");
 
-var recipes = [
-    {
-        name: "Pad Thai",
-        image: "https://www.recipetineats.com/wp-content/uploads/2018/05/Chicken-Pad-Thai_9-landscape-650x501.jpg"
-    },
-    {
-        name: "Asian beef ramen noodle",
-        image: "https://www.recipetineats.com/wp-content/uploads/2018/12/Asian-Beef-Noodles_7-650x813.jpg"
-    },
-    {
-        name: "Cheese muffins",
-        image: "https://www.recipetineats.com/wp-content/uploads/2016/05/Cheesy-Savoury-Muffins_1a.jpg"
-    }
-]
+//SCHEMA SETUP
+var recipeSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+//creates model from Schema to add methods
+var Recipe = mongoose.model("Recipe", recipeSchema);
+
+// Recipe.create(
+//     {
+//         name: "Asian beef ramen noodle",
+//         image: "https://www.recipetineats.com/wp-content/uploads/2018/12/Asian-Beef-Noodles_7-650x813.jpg"
+//     }, function(err, recipe){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log("Newly created recipe: ");
+//         console.log(recipe);
+//     }
+// });
+
+// var recipes = [
+//     {
+//         name: "Pad Thai",
+//         image: "https://www.recipetineats.com/wp-content/uploads/2018/05/Chicken-Pad-Thai_9-landscape-650x501.jpg"
+//     },
+//     {
+//         name: "Asian beef ramen noodle",
+//         image: "https://www.recipetineats.com/wp-content/uploads/2018/12/Asian-Beef-Noodles_7-650x813.jpg"
+//     },
+//     {
+//         name: "Cheese muffins",
+//         image: "https://www.recipetineats.com/wp-content/uploads/2016/05/Cheesy-Savoury-Muffins_1a.jpg"
+//     }
+// ]
 
 app.get("/", function(req,res){
     res.render("landing");
 });
 
 app.get("/recipes", function(req,res){
-    //renders the recipes object, first name could be anything, 2nd is the data to pass
-    res.render("recipes", {recipes:recipes});
+    //Get all recipes from DB
+    Recipe.find({}, function(err, allRecipes){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("recipes", {recipes:allRecipes});
+        }
+    });
+    // res.render("recipes", {recipes:recipes});
 });
 
 app.post("/recipes", function(req, res){
@@ -46,9 +75,14 @@ app.post("/recipes", function(req, res){
     var name = req.body.name;
     var image = req.body.image;
     var newRecipe = {name: name, image: image};
-    recipes.push(newRecipe);
-    //redirect back to recipes page
-    res.redirect("/recipes");
+    //Create a new recipe and save to DB
+    Recipe.create(newRecipe, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else {
+            res.redirect("/recipes");
+        }
+    })
 });
 
 app.get("/recipes/new", function(req, res){
